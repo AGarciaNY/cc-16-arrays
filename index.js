7c34be048efb6622fe286c3f6f0418c67d94ad24
AGarciaNY <aldairgarcia360@gmail.com>
2022-04-28 15:01:25 +0000

    feet: made a function replaceWithYerr

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //write your code here
+//declare a function replaceWithYerr that takes in an array,a start index, and a end index
+//declare an emtey array
+//interate over the given array and replace the giving index with yerr
+//add it to new array
+// return 
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+function replaceWithYerr(array,start = 0 , amount = array.length){
+    let newarray=[];
+    for(let i =0 ; i < array.length ; i++){
+        
+        if(( i  >= start ) && (i < (start + amount ))){
+            newarray.push('yerr');
+        }else{
+            newarray.push(array[i]);
+        }
+    }
+    return newarray;
+}
+console.log(replaceWithYerr(months , 2,2));
